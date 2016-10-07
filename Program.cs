39422523532c7a6e7f1e53dcd35919f33ec4e340
Fred Wang <namelos@outlook.com>
2016-10-07 16:47:47 +0800

    add parking slot and simple parking lot

--- Program.cs
+++ Program.cs
@@ -12,4 +12,36 @@ namespace ParkingLot
         {
         }
     }
+
+    public class ParkingLot
+    {
+        private readonly List<List<Slot>> _parkingLot;
+        public ParkingLot(int row, int column)
+        {
+            _parkingLot = Enumerable.Range(1, row)
+                .Select(_ => Enumerable.Range(1, column)
+                    .Select(__ => new Slot()).ToList()).ToList();
+        }
+
+        public bool Park(int row, int column)
+        {
+            return FindSlot(row, column).HasCar;
+        }
+
+        private Slot FindSlot(int row, int column) => _parkingLot[row][column];
+    }
+
+    public class Slot
+    {
+        public Slot(bool hasCar = false)
+        {
+            HasCar = hasCar;
+        }
+
+        public bool HasCar { get; set; }
+
+        public bool ParkOrWithDraw() => HasCar = !HasCar;
+
+        public override string ToString() => HasCar ? "O" : "X";
+    }
 }
--- TestMain.cs
+++ TestMain.cs
@@ -10,9 +10,57 @@ namespace ParkingLot
     public class TestMain
     {
         [Fact]
-        public void ShouldTest()
+        public void ShouldTest() => Assert.True(true);
+    }
+
+    public class TestParkingLot
+    {
+        private ParkingLot _parkingLot;
+
+        public TestParkingLot()
         {
-            Assert.True(ParkingLot.True());
+            _parkingLot = new ParkingLot(4, 5);
         }
+
+        [Fact]
+        public void ShouldReturnTrueWhenParkingInAnEmptySlotThenPark()
+        {
+            
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenTryParkingASlotWithCar()
+        {
+            
+        }
+    }
+
+    public class TestParking
+    {
+        private readonly Slot _defaultSlot;
+        private readonly Slot _parkedSlot;
+        private readonly Slot _emptySlot;
+
+        public TestParking()
+        {
+            _defaultSlot = new Slot();
+            _parkedSlot = new Slot(true);
+            _emptySlot = new Slot(false);
+        }
+
+        [Fact]
+        public void ShouldNotHasCarByDefault() => Assert.Equal(_defaultSlot.HasCar, false);
+
+        [Fact]
+        public void ShouldShowEmptyParkingAsX() => Assert.Equal(_emptySlot.ToString(), "X");
+
+        [Fact]
+        public void ShouldShowParkedParkingAsX() => Assert.Equal(_parkedSlot.ToString(), "O");
+
+        [Fact]
+        public void ShouldHasCarAfterParkACar() => Assert.Equal(_emptySlot.ParkOrWithDraw(), true);
+
+        [Fact]
+        public void ShouldNotHasCarAfterWithDraw() => Assert.Equal(_parkedSlot.ParkOrWithDraw(), false);
     }
 }
