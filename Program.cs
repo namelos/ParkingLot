1c9c742c3fa0a1fb1da3134f234cdd44c86350c7
Fred Wang <namelos@outlook.com>
2016-10-07 15:01:57 +0800

    add xunit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ParkingLot
 {
---
+++ TestMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLot
+{
+    public class TestMain
+    {
+        [Fact]
+        public void ShouldTest()
+        {
+            Assert.True(ParkingLot.True());
+        }
+    }
+}
